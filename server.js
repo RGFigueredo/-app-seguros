1023288522bcd523fe0f433059aeca4d5b17aed5
F1618156 Benisson dos Santos Lopes <benisson.lopes@bb.com.br>
2020-05-30 18:57:38 -0300

    adicionando cors

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
+const cors = require('cors')
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
+  .use(cors())
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
